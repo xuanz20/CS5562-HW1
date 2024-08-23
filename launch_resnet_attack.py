f646c312a269450d945413dd4322324ad3997b23
Louise Xu <louisexu426@gmail.com>
2024-08-23 17:16:31 +0800

    Upload files

---
+++ launch_resnet_attack.py
@@ -0,0 +1,94 @@
+import argparse
+import os
+from resnet_attack_todo import ResnetPGDAttacker
+from datasets import load_dataset
+from torchvision.models import resnet50, ResNet50_Weights
+from torch.utils.data import DataLoader
+import torch
+
+parser = argparse.ArgumentParser(description="Attacking a Resnet50 model")
+parser.add_argument('--eps', type=float, help='maximum perturbation for PGD attack', default=8 / 255)
+parser.add_argument('--alpha', type=float, help='step size for PGD attack', default=2 / 255)
+parser.add_argument('--steps', type=int, help='number of steps for PGD attack', default=20)
+parser.add_argument('--batch_size', type=int, help='batch size for PGD attack', default=100)
+parser.add_argument('--batch_num', type=int, help='number of batches on which to run PGD attack', default=None)
+parser.add_argument('--results', type=str, help='name of the file to save the results to', required=True)
+parser.add_argument('--resultsdir', type=str, help='name of the folder to save the results to', default='results')
+parser.add_argument('--seed', type=int, help='set manual seed value for reproducibility, default 1234',
+                    default=1234)
+parser.add_argument('--test', action='store_true', help='test that code runs')
+args = parser.parse_args()
+
+RESULTS_DIR = args.resultsdir
+RESULTS_PATH = os.path.join(RESULTS_DIR, args.results)
+if not os.path.isdir(RESULTS_DIR):
+    os.makedirs(RESULTS_DIR)
+
+if args.seed:
+    SEED = args.seed
+    torch.manual_seed(SEED)
+else:
+    SEED = torch.seed()
+
+EPS = args.eps
+ALPHA = args.alpha
+STEPS = args.steps
+
+BATCH_SIZE = args.batch_size
+BATCH_NUM = args.batch_num
+if BATCH_NUM is None:
+    BATCH_NUM = 1281167 // BATCH_SIZE + 1
+assert BATCH_NUM > 0
+
+print('Loading model...')
+# Step 1: Initialize model with the best available weights
+weights = ResNet50_Weights.DEFAULT
+model = resnet50(weights=weights)
+preprocess = weights.transforms()
+
+# Step 2: Load and preprocess data
+print('Loading data...')
+
+# Load ImageNet-1k dataset from Huggingface
+ds = load_dataset("ILSVRC/imagenet-1k", split="train", streaming=True, trust_remote_code=True)
+
+
+def preprocess_img(example):
+    example['image'] = preprocess(example['image'])
+    return example
+
+
+# Filter out grayscale images
+ds = ds.filter(lambda example: example['image'].mode == 'RGB')
+# Preprocess function will be applied to images on-the-fly whenever they are being accessed in the loop
+ds = ds.map(preprocess_img)
+ds = ds.shuffle(seed=SEED)
+# Only take desired portion of dataset
+ds = ds.take(BATCH_NUM * BATCH_SIZE)
+
+dset_loader = DataLoader(ds, batch_size=BATCH_SIZE)
+dset_classes = weights.meta["categories"]
+attacker = ResnetPGDAttacker(model, dset_loader)
+
+if args.test:
+    print(f"===Testing on {BATCH_NUM if BATCH_NUM else "all"} batches of data===")
+    attacker.compute_accuracy(BATCH_NUM)
+    print(f"Accuracy on original images: {attacker.acc * 100}%")
+    torch.save({
+        'acc': attacker.acc,
+    }, RESULTS_PATH)
+
+else:
+    print(f"===Launching PGD attack on {BATCH_NUM if BATCH_NUM else "all"} batches of data===")
+    print(f"Attack configs: eps = {EPS}, alpha = {ALPHA}, steps = {STEPS}, batch size = {BATCH_SIZE}")
+
+    attacker.pgd_batch_attack(EPS, ALPHA, STEPS, BATCH_NUM)
+    print(f"Accuracy on original images: {attacker.acc * 100}%")
+    print(f"Accuracy on adversarial images: {attacker.adv_acc * 100}%")
+
+    torch.save({
+        'acc': attacker.acc,
+        'adv_acc': attacker.adv_acc,
+        'adv_images': attacker.adv_images,
+        'labels': attacker.labels,
+    }, RESULTS_PATH)
---
+++ resnet_attack_todo.py
@@ -0,0 +1,125 @@
+from torch.utils.data import DataLoader
+import torch
+from torch import nn
+from tqdm import tqdm
+
+
+class ResnetPGDAttacker:
+    def __init__(self, model, dataloader: DataLoader):
+        '''
+        The PGD attack on Resnet model.
+        :param model: The resnet model on which we perform the attack
+        :param dataloader: The dataloader loading the input data on which we perform the attack
+        '''
+        self.model = model
+        self.dataloader = dataloader
+        self.batch_size = dataloader.batch_size
+        self.loss_fn = nn.CrossEntropyLoss()
+        self.adv_images = []
+        self.labels = []
+        self.eps = 0
+        self.alpha = 0
+        self.steps = 0
+        self.acc = 0
+        self.adv_acc = 0
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        self.model.to(self.device)
+        # Nullify gradient for model params
+        for p in self.model.parameters():
+            p.requires_grad = False
+
+    def pgd_attack(self, image, label, eps=None, alpha=None, steps=None):
+        '''
+        Create adversarial images for given batch of images and labels
+
+        :param image: Batch of input images on which we perform the attack, size (BATCH_SIZE, 3, 224, 224)
+        :param label: Batch of input labels on which we perform the attack, size (BATCH_SIZE)
+        :return: Adversarial images for the given input images
+        '''
+        if eps is None:
+            eps = self.eps
+        if alpha is None:
+            alpha = self.alpha
+        if steps is None:
+            steps = self.steps
+        images = image.clone().detach().to(self.device)
+        adv_images = images.clone()
+        labels = label.clone().detach().to(self.device)
+
+        # Starting at a uniformly random point within eps ball
+        pass  # TODO
+
+        for _ in range(steps):
+            adv_images.requires_grad = True
+            outputs = self.model(adv_images).softmax(1)
+            # Calculate loss
+            loss = self.loss_fn(outputs, labels)
+            # Compute gradient wrt images
+            grad = torch.autograd.grad(
+                loss, adv_images, retain_graph=False, create_graph=False
+            )[0]
+            adv_images = adv_images.detach()
+            # Gradient update
+            pass  # TODO
+            # Projection step
+            pass  # TODO
+            # Clip image values between 0 and 1
+            adv_images = torch.clamp(adv_images, min=0, max=1).detach()
+
+        return adv_images
+
+    def pgd_batch_attack(self, eps, alpha, steps, batch_num):
+        '''
+        Launch attack for many batches and save results as class features
+        :param eps: Epsilon value in PGD attack
+        :param alpha: Alpha value in PGD attack
+        :param steps: Step value in PGD attack
+        :param batch_num: Number of batches to run the attack on
+        :return: Update attacker accuracy on original images, accuracy on adversarial images,
+        and list of adversarial images
+        '''
+        self.model.eval()
+        self.eps = eps
+        self.alpha = alpha
+        self.steps = steps
+        adv_correct = 0
+        correct = 0
+        total = 0
+        adv_images_lst = []
+        for i, inputs in enumerate(tqdm(self.dataloader, total=batch_num)):
+            if i == batch_num:
+                break
+            adv_images = self.pgd_attack(**inputs)
+            with torch.no_grad():
+                adv_outputs = self.model(adv_images).softmax(1)
+                adv_predictions = adv_outputs.argmax(dim=1).cpu()
+                outputs = self.model(inputs['image'].to(self.device)).softmax(1)
+                predictions = outputs.argmax(dim=1).cpu()
+            labels = inputs['label']
+            adv_correct += torch.sum(adv_predictions == labels).item()
+            correct += torch.sum(predictions == labels).item()
+            total += len(labels)
+            adv_images_lst.append(adv_images)
+        self.adv_images = torch.cat(adv_images_lst).cpu()
+        self.acc = correct / total
+        self.adv_acc = adv_correct / total
+
+    def compute_accuracy(self, batch_num):
+        '''
+        Compute model accuracy for specified number of data batches from self.dataloader
+        :param batch_num: Number of batches on which we compute model accuracy
+        :return: Update model accuracy
+        '''
+        self.model.eval()
+        with torch.no_grad():
+            correct = 0
+            total = 0
+            for i, inputs in enumerate(tqdm(self.dataloader, total=batch_num)):
+                if i == batch_num:
+                    break
+                inputs = {k: v.to(self.device) for (k, v) in inputs.items()}
+                outputs = self.model(inputs['image']).softmax(1)
+                predictions = outputs.argmax(dim=1)
+                correct += (predictions == inputs['label']).sum().item()
+                total += predictions.size(0)
+        self.acc = correct / total
